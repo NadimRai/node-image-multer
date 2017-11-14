74e4af8e8afd0bbf4dddff786c66784e6e690f7a
NadimRai <rai_nadim@yahoo.com>
2017-11-14 14:21:33 +0800

    setup view engine template ejs

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+
+//Init app
+const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
+//Public Folder
+app.use(express.static('./public'));
+
+//Routes
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
